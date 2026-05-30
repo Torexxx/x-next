1c51d9a7115f6699ef45d9d177635a58bdc36da1
torexx <igoninto@tngc.ru>
2026-05-31 01:31:09 +0700

    add styles and mock tweets

--- src/app/(home)/Tweet.tsx
+++ src/app/(home)/Tweet.tsx
@@ -7,10 +7,12 @@ interface Props {
 
 export function Tweet({ tweet }: Props) {
   return (
-    <div>
-      <Image src="/x-logo.svg" alt="X Logo" width={24} height={24} />
-      <p>{tweet.text}</p>
-      <span>@{tweet.author}</span>
+    <div className="border border-white/10 rounded-xl p-4 bg-black text-white shadow-md">
+      <div className="flex items-center gap-3 mb-2">
+        <Image src="/x-logo.svg" alt="X Logo" width={24} height={24} />
+        <span className="font-semibold">@{tweet.author}</span>
+      </div>
+      <p className="text-white/90">{tweet.text}</p>
     </div>
   );
 }
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,12 +1,17 @@
 import { Tweet } from "./Tweet";
 
-
 export default function Home() {
   return (
-    <Tweet tweet={{
-      author: 'Torex',
-      text: 'Rnadom tweet'
-
-    }}/>
+    <div className="min-h-screen flex justify-center px-4 py-8">
+      <div className="w-full max-w-xl">
+        <h1 className="text-3xl font-bold mb-6"> Home</h1>
+        <Tweet
+          tweet={{
+            author: "Torex",
+            text: "Be humble",
+          }}
+        />
+      </div>
+    </div>
   );
 }
